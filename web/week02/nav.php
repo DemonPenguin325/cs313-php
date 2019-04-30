e946b607e62cebe48eec32833cad804fd50ebbad
DemonPenguin325 <demonpenguin325@gmail.com>
2019-04-30 16:04:24 -0600

    Add php pages

---
+++ web/week02/nav.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <h3>AlphaCo.</h3>
+        <ul>
+            <li><a href="./home.php">Home</a></li>
+            <li><a href="./login.php">Login</a></li>
+            <li><a href="./about-us.php">About</a></li>
+        </ul>
+    </nav>
+</header>
