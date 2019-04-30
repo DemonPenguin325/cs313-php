7a5d2bb29701e8c079f9dd3865ccf121bcfe27f8
DemonPenguin325 <demonpenguin325@gmail.com>
2019-04-30 16:14:35 -0600

    Add css

--- web/week02/nav.php
+++ web/week02/nav.php
@@ -2,9 +2,23 @@
     <nav>
         <h3>AlphaCo.</h3>
         <ul>
-            <li><a href="./home.php">Home</a></li>
-            <li><a href="./login.php">Login</a></li>
-            <li><a href="./about-us.php">About</a></li>
+            <?php
+                $home = "";
+                $login = "";
+                $about = "";
+                if (__FILE__ == "home"){
+                    $home = "highlight";
+                }
+                else if (__FILE__ == "about-us"){
+                    $about = "highlight";
+                }
+                else if (__FILE__ == "login"){
+                    $login = "highlight";
+                }
+                echo "<li class='$home'><a href='./home.php'>Home</a></li>";
+                echo "<li class='$login'><a href='./login.php'>Login</a></li>";
+                echo "<li class='$about'><a href='./about-us.php'>About</a></li>";
+            ?>
         </ul>
     </nav>
 </header>
