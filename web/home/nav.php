524b413c4054a4278265cf59e653a38dd8e94461
DemonPenguin325 <demonpenguin325@gmail.com>
2019-05-03 11:06:05 -0600

    Fix multiple home links

--- web/home/nav.php
+++ web/home/nav.php
@@ -7,9 +7,9 @@
       <a class="navbar-brand" href="#">Tristan Fullmer</a>
     </div>
     <ul class="nav navbar-nav">
-      <li <?php if ($file == "home/index.php"){echo 'class="active"';} ?>><a href="./index.php">Home</a></li>
-      <li <?php if ($file == "home/assignments.php"){echo 'class="active"';} ?>><a href="./assignments.php">Assignments</a></li>
-      <li <?php if ($file == "home/other.php"){echo 'class="active"';} ?>><a href="#">Other</a></li>
+      <li <?php if ($file == "/home/index.php"){echo 'class="active"';} ?>><a href="./index.php">Home</a></li>
+      <li <?php if ($file == "/home/assignments.php"){echo 'class="active"';} ?>><a href="./assignments.php">Assignments</a></li>
+      <li <?php if ($file == "/home/other.php"){echo 'class="active"';} ?>><a href="#">Other</a></li>
     </ul>
   </div>
 </nav>
