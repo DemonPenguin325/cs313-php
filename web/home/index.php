ec3e744866c9101b2d672ed3fbf4d659a092c1a7
DemonPenguin325 <demonpenguin325@gmail.com>
2019-05-03 11:13:54 -0600

    Add carousel and Resource folder

--- web/home/index.php
+++ web/home/index.php
@@ -15,6 +15,40 @@
             include 'nav.php';
         ?>
     </header>
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+        <img src="Do not meddle with Dragons.jpg" alt="Do not meddle with Dragons">
+        </div>
+
+        <div class="item">
+        <img src="tolkein.jpg" alt="Adventures with Dragons">
+        </div>
+
+        <div class="item">
+        <img src="battle.jpg" alt="Sword fight">
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
     
 </body>
 </html>
